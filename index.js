bf085434b077004c8e34f88efcbe880922978a75
Android <shafiqshahzaib@gmail.com>
2023-08-03 17:46:02 +0500

    Create Routes and Models

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 
 const port = 3001;
 
-app.get('/',(req,res)=>{
-    res.send('hello')
-})
+app.use('/api/auth',require('./Routes/auth'));
+app.use('/api/notes',require('./Routes/notes'));
+
 app.listen(port, () => {
     console.log(`Connected on : ${port}`);
 });
