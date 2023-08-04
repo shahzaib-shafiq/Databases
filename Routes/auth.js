bc983cc8eef797e1588368e89ee4c191cafa165f
Android <shafiqshahzaib@gmail.com>
2023-08-04 15:51:37 +0500

    added Salt to Password

--- Routes/auth.js
+++ Routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../Models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcrypt');
 
 router.post("/createuser", [
   body('email').isEmail().withMessage('Enter Email'),
@@ -21,10 +22,13 @@ router.post("/createuser", [
       return res.status(400).json({ error: "A user with this email already exists" });
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt); // Declare secPass using 'const' or 'let'
+
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: secPass,
     });
 
     res.json(user);
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectDB = require("./db"); // Make sure the file path to db.js is correct
 const authRouter = require("./Routes/auth");
 
+
+
 const PORT = 3000;
 const app = express();
 app.use(express.json());
