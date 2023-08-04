ff4ed3c2f22a4a0d5c589076e27df64320964d25
Android <shafiqshahzaib@gmail.com>
2023-08-04 12:26:01 +0500

    Added email authentication

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,23 +1,32 @@
 const express = require("express");
 const User = require("../Models/User");
 const router = express.Router();
-const {body, validationResult}= require("express-validator");
+const { body, validationResult } = require("express-validator");
 
-router.post("/auth",[
-    body('email').isEmail().withMessage('Enter Email'),
-    body('name').isLength({ min: 3 }).withMessage('Enter Name'),
-    body('password').isLength({ min: 5 }).withMessage('Enter Password(5 Chracters Min)'),
+router.post("/createuser", [
+  body('email').isEmail().withMessage('Enter Email'),
+  body('name').isLength({ min: 3 }).withMessage('Enter Name'),
+  body('password').isLength({ min: 5 }).withMessage('Enter Password (5 Characters Min)'),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-],(req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user));
+  // check whether user with this email already exists
+  let user = await User.findOne({ email: req.body.email });
+
+  if (user) {
+    return res.status(400).json({ error: "A user with this email already exists" });
+  }
+
+  user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  res.json(user);
 });
 
 module.exports = router;
