6d7679c0233ee48144dd166573653df5974106c1
Android <shafiqshahzaib@gmail.com>
2023-08-05 09:23:13 +0500

    Added JWT Token

--- Routes/auth.js
+++ Routes/auth.js
@@ -6,6 +6,8 @@ const bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 const JWT_SECRET='Harryisagoodb$oy'
 
+
+
 router.post("/createuser", [
   body('email').isEmail().withMessage('Enter Email'),
   body('name').isLength({ min: 3 }).withMessage('Enter Name'),
@@ -39,6 +41,7 @@ router.post("/createuser", [
 
     
     const authtoken=jwt.sign(data,JWT_SECRET);
+    
     // console.log(jwtData)
    
     // res.json(user);
@@ -46,7 +49,47 @@ router.post("/createuser", [
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "An error occurred while creating the user" });
+  
   }
 });
 
-module.exports = router;
+
+router.post("/login", [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password Cannot be blank').exists(),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "Please try to login with correct credentials" });
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      return res.status(400).json({ error: "Please try to login with correct credentials" });
+    }
+
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({ authtoken });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({ error: "An error occurred of Internal Server" });
+  }
+});
+
+
+module.exports = router; 
+
+
